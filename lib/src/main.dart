af85f54e082badaf052bb03249a7714eedc983a1
Joe15975 <43861205+Joe15975@users.noreply.github.com>
2024-01-18 18:48:12 +0200

    removed not important lib

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -3,21 +3,14 @@ import 'package:window_manager/window_manager.dart';
 
 import 'my_screen.dart';
 
-ValueNotifier<bool> isDarkMode = ValueNotifier<bool>(false);
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await windowManager.ensureInitialized();
 
   runApp(
-    ValueListenableBuilder(
-        valueListenable: isDarkMode,
-        builder: (context, value, child) {
-          return MaterialApp(
-              themeMode: isDarkMode.value ? ThemeMode.dark : ThemeMode.light,
-              home: const MyScreen()
-          );
-        },
+    const MaterialApp(
+        home: MyScreen()
     )
   );
 }
--- lib/src/my_screen.dart
+++ lib/src/my_screen.dart
@@ -1,9 +1,7 @@
 
-import 'package:animated_theme_changing/animated_theme_changing.dart';
 import 'package:flutter/material.dart';
 
 import 'garvity/position_calculator.dart';
-import 'main.dart';
 
 
 class MyScreen extends StatefulWidget {
@@ -26,16 +24,10 @@ class _MyScreenState extends State<MyScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedThemeChangerScaffold(
+    return Scaffold(
       appBar: AppBar(
         elevation: 8,
         title: const Text('Flutter - Gravity'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          isDarkMode.value = !isDarkMode.value;
-        },
-        child: const Icon(Icons.lightbulb_outline),
       ),
       body: Center(
         child: ValueListenableBuilder(
@@ -64,7 +56,6 @@ class _MyScreenState extends State<MyScreen> {
           },
         )
       ),
-      isDarkMode: isDarkMode,
     );
   }
 }
